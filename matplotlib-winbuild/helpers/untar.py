60020fd86c61a3da460437a8501a439b8573ccf7
Santos Bañuelos <santos.banuelos93@gmail.com>
2015-12-18 23:18:28 -0600

    algo

--- matplotlib-winbuild/helpers/untar.py
+++
@@ -1,3 +0,0 @@
-import sys, tarfile 
-with tarfile.open(sys.argv[1], 'r:gz') as tgz: 
-    tgz.extractall(sys.argv[2]) 
--- matplotlib-winbuild/helpers/unzip.py
+++
@@ -1,3 +0,0 @@
-import sys, zipfile 
-with zipfile.ZipFile(sys.argv[1]) as zf: 
-    zf.extractall(sys.argv[2]) 
